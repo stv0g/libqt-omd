7f9b9a5043c048ad6f920bd51aa5b4ac03264905
Steffen Vogel <post@steffenvogel.de>
2015-02-10 01:26:38 +0100

    added copyright and Oxygen preambles

--- libqt-omd/image.cpp
+++ libqt-omd/image.cpp
@@ -1,3 +1,28 @@
+/** Oi Image Class
+ *
+ * @copyright	2015 Steffen Vogel
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU Public License
+ * @author      Steffen Vogel <post@steffenvogel.de>
+ * @link        https://dev.0l.de/wiki/projects/omd/start
+ * @package     libqt-omd
+ */
+/*
+ * This file is part of libqt-omd
+ *
+ * libqt-omd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * libqt-omd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libqt-omd. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "camera.h"
 #include "image.h"
 
